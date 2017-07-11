1f59d665ddc9a337276e869c00ab42f6f59a6265
Roey <roeycollignon@gmail.com>
2017-07-11 13:42:33 +0300

    Update and rename V1.py to EnterTheWorkeyPage.py

---
+++ EnterTheWorkeyPage.py
@@ -0,0 +1,5 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome(executable_path='C:/Selenium/chromedriver.exe')
+driver.get('https://www.test.workey.co')
