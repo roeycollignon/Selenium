66926dfacd7e732d3315fc2d0c0b71a6181c5cb9
Roey <roeycollignon@gmail.com>
2017-07-11 14:00:30 +0300

    Update EnterTheWorkeyPage.py

--- EnterTheWorkeyPage.py
+++ EnterTheWorkeyPage.py
@@ -3,3 +3,8 @@ from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome(executable_path='C:/Selenium/chromedriver.exe')
 driver.get('https://www.test.workey.co')
+
+#button =  driver.find_element_by_class_name("btn btn-new-brand btn-lg margin-top-medium btn btn-default")
+button =  driver.find_element_by_css_selector("button.btn.btn-new-brand.btn-lg.margin-top-medium.btn.btn-default")
+
+print button.text
